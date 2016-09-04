1c1658836e26cd24612b9aa937ef32e013bc1fb1
fytsai <fangyit@gmail.com>
2016-09-04 21:43:30 +0800

    finished version at 2016/09/04

--- server.R
+++ server.R
@@ -3,24 +3,27 @@ library(shiny)
 library(ggplot2)
 library(scales)
 Sys.setlocale("LC_TIME", "English")
+#load caffeine data
 coffee <- read.csv("stcf.csv", stringsAsFactors = FALSE)
 
 server <- function(input, output) {
+    # Calculate caffeine according to input selected.
+    # Model assumed a 11% decay of caffeine every hour. 
+    # When drinking more than 1 cup, effects will be add to data(), starting from the time taken.
+    # The time axis (x-axis) is in POSIXct format, assuming date of today.
     data <- reactive({
-        data.frame(
-            Hours= seq(from=0, to =24, by=1)*3600+strptime(paste(as.Date(Sys.time()), input$drinkTime), format="%Y-%m-%d %H"), 
-            Caffeine= coffee$Caffeine[which(coffee$Beverage==input$coffee & coffee$Size==input$size)]*0.89^seq(from=0, to =24, by=1)
-        )
+            data.frame(
+                Hours= seq(from=0, to =24, by=1)*3600+strptime(paste(as.Date(Sys.time()), input$drinkTime), format="%Y-%m-%d %H"),
+                Caffeine= as.numeric(input$morning==TRUE)*coffee$Caffeine[which(coffee$Beverage==input$coffee & coffee$Size==input$size)]*0.89^seq(from=0, to =24, by=1)
+                    + as.numeric(input$afternoon==TRUE)*append(head(coffee$Caffeine[which(coffee$Beverage==input$coffee2 & coffee$Size==input$size2)]*0.89^seq(from=0, to =24, by=1), input$drinkTime+25-input$drinkTime2), rep(0,input$drinkTime2-input$drinkTime),after=0) 
+                    + as.numeric(input$evening==TRUE)*append(head(coffee$Caffeine[which(coffee$Beverage==input$coffee3 & coffee$Size==input$size3)]*0.89^seq(from=0, to =24, by=1), input$drinkTime+25-input$drinkTime3), rep(0,input$drinkTime3-input$drinkTime),after=0)
+            )
 
     })
-
+    
+    # Plot of reactive data(), and horizontal line at 50mg.
     output$decay <- renderPlot({
-        # plot(data()$x,
-        #      data()$y,
-        #      type="l",
-        #      ylab="Caffeine in Body (mg)",
-        #      xlab="Hours Since Consuming Coffee")
-        ggplot(data(), aes(x=Hours, y=Caffeine)) + geom_line() + xlab("") + ylab("Caffeine")#+ scale_x_datetime(labels=date_format('%H:%M')) 
+        ggplot(data(), aes(x=Hours, y=Caffeine)) + geom_line() + xlab("") + ylab("Caffeine (mg)") + scale_y_continuous(breaks=seq(0, 500, 10)) + scale_x_datetime(breaks = date_breaks("1 hour")) + theme(axis.text.x  = element_text(angle=90, vjust=0.5, size=16)) + theme(axis.title.y = element_text(size = rel(1.5), angle = 90)) + geom_hline(yintercept=50, color="red", linetype="dashed")
             
     })
 }
--- ui.R
+++ ui.R
@@ -1,20 +1,87 @@
 #ui.R
 library(shiny)
-coffee <- read.csv("stcf.csv", stringsAsFactors = FALSE)
+#load caffeine data
+coffee <- read.csv("stcf.csv", stringsAsFactors = FALSE) 
 ui <- fluidPage(
-    selectInput("coffee", 
-                label = "Choose a coffee",
-                choices = unique(coffee$Beverage), 
-                selected = "Caffe Misto"),
-    selectInput("size", 
-                label = "Choose a size",
-                choices = c("Short","Tall","Grande","Venti"), 
-                selected = "Tall"),
-    sliderInput(inputId = "drinkTime",
-                label = "Consumed at what Hour",
-                min = 0,
-                max = 23,
-                value = 6,
-                step = 1),
+    title = "Caffeine Calculator",
+    h1("Caffeine Calculator"),
+    h4("This app calculates how much caffeine remains in the body throughout the day."),
+    h4("The default assumes a cup of coffee is consumed in the morning. The black curve in the plot is the caffeine (mg) remaining."),
+    h4("Choices of coffee, size, and time consumed can be changed, and the plot will update accordingly."),
+    h4("If coffee is also taken in the afternoon/evening, check the corresponding checkbox, and the effect will be added to the plot."),
+    h4("A red horizontal line marks 50mg of caffeine. This is to remind that if the level of caffeine is higher than this line when going to bed, the quality of sleep may be affected."),
+    hr(),
+    
+    fluidRow(
+        # display choices for morning/afternoon/evening over 3 columns of the same row
+        column(4,
+               # default is coffee taken in the morning
+               checkboxInput(inputId="morning", label="Morning", TRUE),
+               # Only show this panel if morning is checked
+               conditionalPanel(
+                   condition = "input.morning == true",
+                   selectInput("coffee",
+                               label = "Choose a coffee",
+                               choices = unique(coffee$Beverage),
+                               selected = "Caffe Misto"),
+                   selectInput("size",
+                               label = "Choose a size",
+                               choices = c("Short","Tall","Grande","Venti"),
+                               selected = "Tall"),
+                   sliderInput(inputId = "drinkTime",
+                               label = "Consumed at what Hour",
+                               min = 0,
+                               max = 12,
+                               value = 6,
+                               step = 1)
+               )
+        ),
+        column(4,
+               checkboxInput(inputId="afternoon", label="Afternoon", FALSE),
+               # Only show this panel if afternoon is checked
+               conditionalPanel(
+                   condition = "input.afternoon == true",
+                   selectInput("coffee2",
+                               label = "Choose a coffee",
+                               choices = unique(coffee$Beverage),
+                               selected = "Caffe Misto"),
+                   selectInput("size2",
+                               label = "Choose a size",
+                               choices = c("Short","Tall","Grande","Venti"),
+                               selected = "Tall"),
+                   sliderInput(inputId = "drinkTime2",
+                               label = "Consumed at what Hour",
+                               min = 13,
+                               max = 18,
+                               value = 14,
+                               step = 1)
+               )
+        ),
+        column(4,
+               checkboxInput("evening", "Evening", FALSE),
+               # Only show this panel if Evening is checked
+               conditionalPanel(
+                   condition = "input.evening == true",
+                    selectInput("coffee3",
+                                label = "Choose a coffee",
+                                choices = unique(coffee$Beverage),
+                                selected = "Caffe Misto"),
+                    selectInput("size3",
+                                label = "Choose a size",
+                                choices = c("Short","Tall","Grande","Venti"),
+                                selected = "Tall"),
+                   sliderInput(inputId = "drinkTime3",
+                               label = "Consumed at what Hour",
+                               min = 19,
+                               max = 23,
+                               value = 20,
+                               step = 1)
+
+               )
+        )
+    ),
+    
+    hr(),
     plotOutput("decay")
+    
 )
